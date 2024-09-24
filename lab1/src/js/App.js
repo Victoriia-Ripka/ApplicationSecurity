51408edf3e6faf90e37c223c9691bfe6ca0a540b
Victoriia-Ripka <vikaripka@gmail.com>
2024-09-24 12:21:14 +0200

    start

---
+++ lab1/src/js/App.js
@@ -0,0 +1,31 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <body>
+      <header>
+        <a href="/" className="logo">Caesar's Cipher</a>
+        <nav>
+          <ul>
+            <li><a href="/files">files</a></li>
+            <li><a href="/encryption">encryption</a></li>
+            <li><a href="/decryption">decryption</a></li>
+            <li><a href="/author">author</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+
+      </main>
+
+      <footer>
+        <div>
+          <p>© 2024 Project by Viktoriia Nowotka</p>
+        </div>
+      </footer>
+    </body>
+  );
+}
+
+export default App;
