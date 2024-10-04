69e651c2999d33bf881cf4e395beaa2735ea9286
Victoriia-Ripka <vikaripka@gmail.com>
2024-10-04 15:06:09 +0200

    lab1 - CaesarCipher class

--- lab1_v2/main.py
+++ lab1_v2/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox, filedialog, simpledialog
 import os
 import string
+from caesarCipher import CaesarCipher
 
 
 BASE_DIR = os.path.dirname(__file__)
@@ -12,56 +13,50 @@ if not os.path.exists(FILES_DIR):
 
 
 class Cryptographic_system:
-    eng_lower = string.ascii_lowercase  
-    eng_upper = string.ascii_uppercase  
-    
-    ukr_lower = 'абвгґдеєжзийіїклмнопрстуфхцчшщьюя'  
-    ukr_upper = 'АБВГҐДЕЄЖЗИЙІЇКЛМНОПРСТУФХЦЧШЩЬЮЯ'
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Криптографічна система")
 
-    def __init__(this):
-        this.root = Tk()
-        this.root.title("Криптографічна система")
-
-        menubar = Menu(this.root)
+        menubar = Menu(self.root)
 
         # Меню для роботи з файлами
         file_menu = Menu(menubar, tearoff=0)
 
-        file_menu.add_command(label="Створити і Зберегти", command=this.save_file)
-        file_menu.add_command(label="Відкрити і Друкувати", command=this.open_file)
+        file_menu.add_command(label="Створити і Зберегти", command=self.save_file)
+        file_menu.add_command(label="Відкрити і Друкувати", command=self.open_file)
         menubar.add_cascade(label="Файл", menu=file_menu)
 
         # Меню для шифрування та розшифрування
         encryption_menu = Menu(menubar, tearoff=0)
 
-        encryption_menu.add_command(label="Зашифрувати файл", command=this.encrypt_file)
-        encryption_menu.add_command(label="Розшифрувати файл", command=this.decrypt_file)
-        encryption_menu.add_command(label="Розшифрувати файл перебором", command=this.decrypt_file_without_key)
+        encryption_menu.add_command(label="Зашифрувати файл", command=self.encrypt_file)
+        encryption_menu.add_command(label="Розшифрувати файл", command=self.decrypt_file)
+        encryption_menu.add_command(label="Розшифрувати файл перебором", command=self.decrypt_file_without_key)
         menubar.add_cascade(label="Шифрування", menu=encryption_menu)
 
         # Інформація про розробника
-        menubar.add_cascade(label="Розробник", command=lambda: this.root.event_generate("<<OpenDeveloperInfo>>"))
+        menubar.add_cascade(label="Розробник", command=lambda: self.root.event_generate("<<OpenDeveloperInfo>>"))
 
         # Вихід з програми
-        menubar.add_cascade(label="Вихід", command=this.root.quit)
+        menubar.add_cascade(label="Вихід", command=self.root.quit)
 
         # Прив'язка меню до головного вікна
-        this.root.config(menu=menubar)
+        self.root.config(menu=menubar)
 
         # Прив'язка події для показу інформації про розробника
-        this.root.bind("<<OpenDeveloperInfo>>", this.launchDeveloperInfo)
+        self.root.bind("<<OpenDeveloperInfo>>", self.launchDeveloperInfo)
 
-        this.root.mainloop()
+        self.root.mainloop()
 
 
-    def open_file(this, *args):
+    def open_file(self, *args):
         file_path = filedialog.askopenfilename(initialdir=FILES_DIR, filetypes=[("Text files", "*.txt")])
         if file_path:
             with open(file_path, 'r', encoding='utf-8') as f:
                 messagebox.showinfo("Друкування файлу", f.read())
 
 
-    def save_file(this, *args):
+    def save_file(self, *args):
         user_message = simpledialog.askstring("Введіть повідомлення", "Введіть текст для запису у файл:")
 
         if not user_message:
@@ -76,34 +71,19 @@ class Cryptographic_system:
             messagebox.showinfo("Збереження", "Файл успішно збережено!")
 
 
-    def shift_char(this, x, n_alphabet, key, *args):
-        if x in n_alphabet:
-            index = n_alphabet.index(x)
-            return n_alphabet[(index + key) % len(n_alphabet)]
-        return x
-
-
-    def encrypt_text(this, text, key, *args):
-        encrypted_text = ''
-        for c in text:
-            if c in this.eng_lower:
-                encrypted_text += this.shift_char(c, this.eng_lower, key)
-            elif c in this.eng_upper:
-                encrypted_text += this.shift_char(c, this.eng_upper, key)
-            elif c in this.ukr_lower:
-                encrypted_text += this.shift_char(c, this.ukr_lower, key)
-            elif c in this.ukr_upper:
-                encrypted_text += this.shift_char(c, this.ukr_upper, key)
-            else:
-                encrypted_text += c
+    def encrypt_text(self, text, key, *args):
+        cipher = CaesarCipher(key)
+        encrypted_text = cipher.encrypt(text)
         return encrypted_text
 
 
-    def decrypt_text(this, text, key, *args):
-        return this.encrypt_text(text, -key)
+    def decrypt_text(self, text, key, *args):
+        cipher = CaesarCipher(key)
+        decrypted_text = cipher.decrypt(text)
+        return decrypted_text
 
 
-    def encrypt_file(this, *args):
+    def encrypt_file(self, *args):
         file_path = filedialog.askopenfilename(initialdir=FILES_DIR, filetypes=[("Text files", "*.txt")])
         if file_path:
             try:
@@ -115,7 +95,7 @@ class Cryptographic_system:
                     messagebox.showwarning("Скасовано", "Шифрування скасовано, оскільки не введено ключ.")
                     return
 
-                encrypted_data = this.encrypt_text(data, key)
+                encrypted_data = self.encrypt_text(data, key)
 
                 with open(file_path, 'w', encoding='utf-8') as f:
                     f.write(encrypted_data)
@@ -126,7 +106,7 @@ class Cryptographic_system:
                 messagebox.showerror("Помилка", f"Не вдалося зашифрувати файл: {e}")
 
 
-    def decrypt_file(this, *args):
+    def decrypt_file(self, *args):
         file_path = filedialog.askopenfilename(initialdir=FILES_DIR, filetypes=[("Text files", "*.txt")])
         if file_path:
             try:
@@ -138,7 +118,7 @@ class Cryptographic_system:
                     messagebox.showwarning("Скасовано", "Шифрування скасовано, оскільки не введено ключ.")
                     return
 
-                decrypted_data = this.decrypt_text(data, key)
+                decrypted_data = self.decrypt_text(data, key)
 
                 with open(file_path, 'w', encoding='utf-8') as f:
                     f.write(decrypted_data)
@@ -149,11 +129,11 @@ class Cryptographic_system:
                 messagebox.showerror("Помилка", f"Не вдалося розшифрувати файл: {e}")
 
 
-    def decrypt_file_without_key(this, *args):
+    def decrypt_file_without_key(self, *args):
         pass
 
 
-    def launchDeveloperInfo(this, *args):
+    def launchDeveloperInfo(self, *args):
         messagebox.showinfo(message="Розробник: Новотка Вікторія Іванівна, група ТВ-13")
 
 
